749c507d815ae51b85377c77721d91ebaf501fb9
Zachary Rener <me@zacharyrener.com>
2020-08-11 15:31:07 -0500

    Initial files

---
+++ app/Controllers/FrontPage.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controllers;
+
+use Sober\Controller\Controller;
+
+class FrontPage extends Controller
+{
+
+    public static $testing = "testing here!!!";
+
+}
---
+++ resources/views/partials/page-header.blade.php
@@ -0,0 +1,5 @@
+<div class="page-header">
+  <h1>{!! App::title() !!}</h1>
+  <p>{!! App::$test !!}</p>
+  <p>{!! FrontPage::$testing !!}</p>
+</div>
